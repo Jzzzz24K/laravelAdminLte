d1f643e23b2adb7139f12a5bd99260b2130c49f2
jingze <877440533@qq.com>
2019-10-27 19:16:53 +0800

    init

---
+++ resources/js/app.js
@@ -0,0 +1,16 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+require('admin-lte');
+require('admin-lte/bower_components/raphael/raphael.min.js');
+require('admin-lte/bower_components/morris.js/morris.min.js');
+require('admin-lte/bower_components/jquery-knob/dist/jquery.knob.min.js');
+require('admin-lte/bower_components/moment/min/moment.min.js');
+require('admin-lte/bower_components/bootstrap-daterangepicker/daterangepicker.js');
+require('admin-lte/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js');
