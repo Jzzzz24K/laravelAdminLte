cc2d1dedf677abd81f3ab57db0a97a9b4a8bbb18
jingze <877440533@qq.com>
2019-11-07 17:03:58 +0800

    角色编辑删除功能

---
+++ resources/views/user/create.blade.php
@@ -0,0 +1,54 @@
+<div class="modal fade in" id="modal-create" style="padding-top: 100px;">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header" style="background-color: #337ab7">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span></button>
+                <h4 class="menu-title modal-title" style="color: white;">创建角色</h4>
+            </div>
+            <form class="menu-form form-horizontal" action="/role" method="post">
+                <div class="modal-body">
+                    <!-- /.box-header -->
+                    <!-- form start -->
+                    <input type="hidden" name="_token" value="{{csrf_token()}}">
+                    <div class="box-body">
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label">角色名称</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="name" class="form-control" placeholder="名称"
+                                       value="{{$fields['name']}}">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="" class="col-sm-2 control-label">角色描述</label>
+                            <div class="col-sm-10">
+                                <input  type="text" name="description" class="form-control" placeholder="描述"
+                                        value="{{$fields['description']}}">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputPassword3" class="col-sm-2 control-label">选择权限</label>
+                            <div class="col-sm-10">
+                                <select id="select2" name="permission_id[]" multiple="multiple" class="form-control" data-placeholder="选择权限" style="width: 100%;height: 34px;">
+                                    @foreach($permissions as $permission)
+                                        <option value="{{$permission->id}}">{{$permission->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-info pull-left" data-dismiss="modal" style="background-color: #337ab7">关闭</button>
+                    <button type="submit" class="btn btn-info btn-primary" style="background-color: #337ab7">保存</button>
+                </div>
+            </form>
+
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+
---
+++ resources/views/user/delete.blade.php
@@ -0,0 +1,20 @@
+<div class="modal modal-danger fade in" id="modal-danger" style="padding-top: 100px;">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span></button>
+                <h4 class="modal-title">危险</h4>
+            </div>
+            <div class="modal-body">
+                <p>确定要删除<kbd><span id="user">file</span></kbd>这个用户吗?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">关闭</button>
+                <button type="button" class="btn btn-outline">删除</button>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
